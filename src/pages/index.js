601db0c1fa3f630049eef7eaaf18282f9411cb1f
bklaing2 <43453735+bklaing2@users.noreply.github.com>
2020-01-27 12:26:01 -0600

    Finished landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,32 @@
-import React from "react"
-import { Link } from "gatsby"
+import { Link } from 'gatsby'
+import React from 'react'
+import indexStyles from './index.module.css'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+
+
+const A = ({ to, children }) => <a href={to}>{children}</a>
+
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi peeps</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div class='row'>
+      <div class='container' style={{flexDirection: 'row'}}>
+        <div style={{width: '50%', alignItems: 'left', padding: '1rem'}}>
+          <h1>Howdy!</h1>
+          I am a Computer Engineering student at <A to='https://www.tamu.edu/'>Texas A&amp;M Univeristy</A>.<br/>
+          This website is a personal project of mine. I wanted to learn some web development, and it was important to me that the end result be something meaningful.<br/><br/>
+          Feel free to take a look around!<br/><br/>
+
+          <h3>After Graduation</h3>
+          I am seeking a job in Computer Engineering or related fields. You can find more information <Link to='/professional-image/'>here</Link> about my qualifications.<br/>
+          If you are looking to hire, I would appreciate being considered. Here's my <Link to='/resume/'>resume</Link>!<br/><br/><br/>
+
+          <h2 class={`${indexStyles.disabled}`}>- Bryceson Laing</h2>
+        </div>
+        <div class={`${indexStyles.headshot}`}></div>
+      </div>
     </div>
-    <p>Now go build something great.</p>
-    <h1>Hi peeps</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <h1>Hi peeps</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <h1>Hi peeps</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
