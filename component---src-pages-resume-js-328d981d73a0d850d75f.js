44fb7a25401acf613edd0b935b7ae8c4424c2310
bryceson-laing <bryceson_laing@tamu.edu>
2021-06-21 08:47:00 -0700

    Built for GitHub Pages

---
+++ component---src-pages-resume-js-328d981d73a0d850d75f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{F2CN:function(e,n,t){"use strict";t.r(n);var r=t("q1tI"),a=t.n(r),o=(t("h0Tq"),t("Bl7J"));n.default=function(){return a.a.createElement(o.a,null,a.a.createElement("iframe",{src:"/resume_bryceson-laing.pdf",allowtransparency:"true",style:{width:"100%",minHeight:1250,border:"none",alignSelf:"center"}}))}},h0Tq:function(e,n,t){e.exports={headshot:"resume-module--headshot--16RBu"}}}]);
+//# sourceMappingURL=component---src-pages-resume-js-328d981d73a0d850d75f.js.map
